7fdf5bd41605d2335eacde31dc465e0033c79167
ricksonporto <rickson.porto@hotmail.com>
2023-12-22 19:46:20 -0300

    first commit

---
+++ desafio4.js
@@ -0,0 +1,34 @@
+function calcularNivel(vitorias, derrotas) {
+    // Calcula o saldo de Rankeadas
+    let saldoVitorias = vitorias - derrotas;
+
+    // Determina o nível com base no saldo de vitórias
+    let nivel;
+    if (vitorias < 10) {
+        nivel = 'Ferro';
+    } else if (vitorias >= 11 && vitorias <= 20) {
+        nivel = 'Bronze';
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = 'Prata';
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = 'Ouro';
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = 'Diamante';
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = 'Lendário';
+    } else {
+        nivel = 'Imortal';
+    }
+
+    // Exibe a mensagem
+    console.log(`O Herói tem um saldo de ${saldoVitorias} e está no nível de ${nivel}`);
+    
+    // Retorna o resultado
+    return {
+        saldoVitorias: saldoVitorias,
+        nivel: nivel
+    };
+}
+
+// Exemplo de uso
+calcularNivel(30, 10);
